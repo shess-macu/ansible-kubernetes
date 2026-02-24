511a7b0188b019713486180fced930aba60f1c21
William Cooke <cookewe@gmail.com>
2026-02-24 12:14:52 +0000

    Remove trimspace

--- ci-cd/test/tofu/virtual-machine/virtual-machine.tf
+++ ci-cd/test/tofu/virtual-machine/virtual-machine.tf
@@ -102,7 +102,7 @@ resource "kubernetes_manifest" "virtual-machine" {
               {
                 guest = var.memory_size
               },
-              trimspace(coalesce(var.hugepages_page_size, "")) != "" ? {
+              coalesce(var.hugepages_page_size, "") != "" ? {
                 hugepages = {
                   pageSize = var.hugepages_page_size
                 }
